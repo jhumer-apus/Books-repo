22bad3de86126f39522a7c6fb6e4898a4281e825
Jhumer Apus <jhumerapus@gmail.com>
2022-08-19 15:54:40 -0700

    default layout added on books page

--- frontend/pages/AboutUs.vue
+++ frontend/pages/AboutUs.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
     <h1 class="text-3xl text-center mt-20">About Us</h1>
+    <nuxt />
   </div>
 </template>
 
 <script>
 
 export default {
-
+  layout:'default'
 }
 </script>
 
--- frontend/pages/Books.vue
+++ frontend/pages/Books.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
     <h1 class="text-3xl text-center mt-20">Books</h1>
+    <nuxt />
   </div>
 </template>
 
 <script>
 export default {
-
+  layout:'default'
 }
 
 
