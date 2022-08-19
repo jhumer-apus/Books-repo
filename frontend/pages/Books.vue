e89aa5f5baaec51ab4291d9657ded67765e17436
Jhumer Apus <jhumerapus@gmail.com>
2022-08-19 15:35:11 -0700

    default layout add components

--- frontend/pages/Books.vue
+++ frontend/pages/Books.vue
@@ -1,28 +1,15 @@
 <template>
   <div>
-    <Header />
-    <SideNav />
     <h1 class="text-3xl text-center mt-20">Books</h1>
-    <Body />
-    <Footer />
   </div>
 </template>
 
 <script>
-import Header from '~/components/Header.vue';
-import Footer from '~/components/Footer.vue';
-import SideNav from '~/components/SideNav.vue';
-import Body from '~/components/Body.vue';
-
 export default {
-    components:{
-    Header,
-    SideNav,
-    Body,
-    Footer
-}
 
 }
+
+
 </script>
 
 <style>
